e89763a229cb5b9f62bb1aa32f3c0e580aa12343
Costinel22 <malaiasi_costinel@yahoo.com>
2024-03-06 21:43:30 +0000

    add about me and avatar on home

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -2,24 +2,20 @@ import React from 'react';
 import Container from '../Container';
 import Row from '../Row';
 import Col from '../Col';
+import myAvatar from '../../image/avatar.jpg';
 
 function AboutMe() {
   return (
     <div>
-      <Container style={{ marginTop: 30 }}>
+      <Container style={{ marginTop: 30, marginLeft: 200}}>
         <Row>
-          <Col size="md-12">
-            <h1>A short inrtoduction about me!</h1>
+          <Col size="md-4 just">
+            <h1>Hello, I am a Web Developrer whit JavaScript.</h1>
+            <br />
+            <h4>I like challenges and recently I started studying web development using javascript plus different libraries, among these are bootstrap, jQuary, React and more. </h4>
           </Col>
-        </Row>
-        <Row>
-          <Col size="md-12">
-            <p>
-              pppppppppppppppppppppppppppppppppppppppppp
-            </p>
-            <p>
-              pppppppppppppppppppppppppppppppppppppppppp
-            </p>
+          <Col size="md-4 just">
+          <img src={myAvatar} alt="My Image avatar" />
           </Col>
         </Row>
       </Container>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import Hero from '../components/Hero';
 import myHero from '../image/coffe.jpg'
 import './style.css'
+import Row from '../components/Row'
+import Col from '../components/Col'
+import Container from '../components/Container';
+import myAvatar from '../image/avatar2.jpg';
 
 function About() {
   return (
     <div>
       <Hero backgroundImage={myHero}>
-        <h1 id="setName">Gheorgh Costinel Malaiasi</h1>
-        <h2><a href="https://www.linkedin.com/in/costinel-malaiasi-58510a249">Linkedin</a></h2>
-        <h2><a href="https://github.com/Costinel22">GitHub</a></h2>
+      <Container style={{ marginTop: 30 }} size="md-12 just">
+        <Row>
+          <Col size="md-6 just">
+          <h1 id="setName">Gheorghe Costinel Malaiasi</h1>
+          <h2><a href="https://www.linkedin.com/in/costinel-malaiasi-58510a249">Linkedin</a></h2>
+          <h2><a href="https://github.com/Costinel22">GitHub</a></h2>
+          </Col>
+          <Col size="md-6 just">
+          <img src={myAvatar} alt="My Image avatar" style={{ marginTop: 30, width: 260}} />
+          </Col>
+        </Row>
+     </Container>
       </Hero>
     </div>
   );
