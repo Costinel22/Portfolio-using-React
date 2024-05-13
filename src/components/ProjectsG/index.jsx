b4f0f6db776248603bb3b93c1823d7bc24b74a38
Costinel22 <malaiasi_costinel@yahoo.com>
2024-05-13 15:27:09 +0100

    add space undder projects

--- src/components/ProjectsG/index.jsx
+++ src/components/ProjectsG/index.jsx
@@ -4,6 +4,7 @@ import "./style.css";
 import React, { useEffect, useState } from 'react';
 import dataS from "../../../data.json";
 import Row from "../Row";
+import Col from "../Col";
 
 
 
@@ -23,37 +24,32 @@ function Projects() {
   }, []);
 
   return (
-    <section className="bigBox" >
-    <Row col-md-6 >
-      <Container style={{}}>
-        <div >
-        {data.map((item, index) => (
-          <Row key={index}>
-            <h2 style={{ color: "black" }}>{item.name}</h2>
-            <div>
-              <img src={item.image} alt={item.name} style={{ width: 340, color: "black" }} />
-            </div>
-            <div style={{width: 360 }}>
-              <p>{item.description}</p>
-            </div>
-            <ul>
-              <li>
-                <a href={item.URL}>Project Link</a>
-              </li>
-              <li>
-                <a href={item.gitHubURL}>GitHub Link</a>
-              </li>
-            </ul>
-            <br />
-            <br />
-            <br />
-            <br />
-          </Row>
-        ))}
-        </div>
+    <section style={{ justifyContent: "space-between", width: 360 }}>
+      <Container style={{ margin: 20 }}>
+        <Row style={{ justifyContent: "space-between" }}>
+          {data.map((item, index) => (
+            <Col md={6} style={{ margin: 20 }} key={index} >
+              <div className="bigBox">
+                <h2 style={{ color: "black", margin: 5 }}>{item.name}</h2>
+                <div>
+                  <img src={item.image} alt={item.name} style={{ width: 340, color: "black", margin: 6}} />
+                </div>
+                <div style={{ width: 340 }}>
+                  <p style={{ color: "black" , margin: 6}}>{item.description}</p>
+                </div>
+                <ul>
+                  <li>
+                    <a href={item.URL} style={{ margin: 5 }}>Project Link</a>
+                  </li>
+                  <li>
+                    <a href={item.gitHubURL } style={{ margin: 5 }}>GitHub Link</a>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+          ))}
+        </Row>
       </Container>
-     </Row>
-     <br />
     </section>
   );
 }
